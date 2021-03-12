fc56c03e46355565c41413140c78789ae3d6f4b3
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-03-12 17:38:04 +0700

    add file Student.cs

---
+++ Hello/Hello/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    public class Student
+    {
+        private int id;
+        private string name;
+
+        public Student() { }
+
+        public Student(int _id, string _name)
+        {
+            this.id = _id;
+            this.name = _name;
+        }
+    }
+}
